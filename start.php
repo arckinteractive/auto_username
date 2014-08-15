1e7a65245d6a9f2c1c89a36e5b14af50983dd3a3
Matt Beckett <beck24@gmail.com>
2014-08-14 21:25:40 -0700

    remove unnecessary event registration

--- start.php
+++ start.php
@@ -6,7 +6,6 @@ elgg_register_event_handler('init', 'system', __NAMESPACE__ . '\\init');
 
 function init() {
 	elgg_register_plugin_hook_handler('action', 'register', __NAMESPACE__ . '\\register_action');
-	elgg_register_event_handler('create', 'user', __NAMESPACE__ . '\\create_user');
 }
 
 
